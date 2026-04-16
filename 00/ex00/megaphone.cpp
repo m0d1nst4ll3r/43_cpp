4c04345a2f1c7d5bbacc3c01373b8e365c857131
m0d1nst4ll3r <raphaelpohlen@gmail.com>
2026-04-16 16:50:58 +0200

    Added c++98 to compile flags, revamped megaphone.cpp to use string methods and have better print logic

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -4,6 +4,8 @@
 
 namespace
 {
+	const std::string whitespace = " \t\n\r";
+
 	std::string	transformToUppercase(std::string str)
 	{
 		for (std::string::iterator it = str.begin(); it != str.end(); ++it)
@@ -11,42 +13,34 @@ namespace
 		return str;
 	}
 
-	std::string	truncateSpaces(const std::string &str)
+	std::string	trimSpaces(const std::string &str)
 	{
-		std::size_t	end;
-		std::size_t	begin;
-
-		if (str.empty())
+		std::size_t start = str.find_first_not_of(whitespace);
+		if (start == std::string::npos)
 			return "";
-		for (begin = 0; str[begin] == ' '; ++begin)
-			; // Get begin
-		if (!str[begin])
-			return std::string();
-		for (end = str.size() - 1; str[end] == ' '; --end)
-			; // Get end
-		return str.substr(begin, end - begin + 1);
+
+		std::size_t end = str.find_last_not_of(whitespace);
+		return str.substr(start, end - start + 1);
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	if (argc < 2)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-	else
+	bool	firstPrinted = false;
+	for (int i = 1; i < argc; ++i)
 	{
-		bool	firstPrinted = false;
-		for (int i = 1; i < argc; ++i)
+		std::string	str = transformToUppercase(argv[i]);
+		str = trimSpaces(str);
+		if (!str.empty())
 		{
-			std::string	str = transformToUppercase(argv[i]);
-			str = truncateSpaces(str);
-			if (!str.empty())
-			{
-				if (firstPrinted)
-					std::cout << ' ';
-				std::cout << str;
-				firstPrinted = true;
-			}
+			if (firstPrinted)
+				std::cout << ' ';
+			std::cout << str;
+			firstPrinted = true;
 		}
-		std::cout << '\n';
 	}
+	if (firstPrinted)
+		std::cout << '\n';
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 }
