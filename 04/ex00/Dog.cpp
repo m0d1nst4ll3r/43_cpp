a72701694d46e5da15527bf5088ced3cee42b3aa
rapohlen <rapohlen@f4r13s2.paris.42.school>
2026-05-24 17:14:15 +0200

    Fedora has new cpp compiler which gives new errors. ex00 fixed, ex01 still broken

--- 04/ex00/Dog.cpp
+++ 04/ex00/Dog.cpp
@@ -16,7 +16,7 @@ void	Dog::makeSound( void ) const
 	std::cout << "Dog barks\n";
 }
 
-Dog::Dog( const Dog& toCopy )
+Dog::Dog( const Dog& toCopy ) : Animal(toCopy)
 {
 	(void)toCopy;
 	std::cerr << "Dog copied\n";
--- 04/ex00/main.cpp
+++ 04/ex00/main.cpp
@@ -27,7 +27,7 @@ int	main( void )
 	std::cout << "\nWrongCat as WrongCat:\n\n";
 
 	{
-		WrongCat		cat;
+		const WrongCat		cat;
 		std::cout << cat.getType() << '\n';
 		cat.makeSound();
 	}
