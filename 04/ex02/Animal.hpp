9faef97d121cacbbd15d01947f86ceca8eebeb6b
rapohlen <rapohlen@f2r8s6.paris.42.school>
2026-05-26 15:14:57 +0200

    ex02 fixed

--- 04/ex02/Animal.hpp
+++ 04/ex02/Animal.hpp
@@ -10,7 +10,7 @@ class Animal {
 	Animal( void );
 	virtual ~Animal( void );
 	Animal( const Animal& toCopy );
-	virtual Animal&	operator=( const Animal& op );
+	Animal&	operator=( const Animal& op );
 
 	std::string	getType( void ) const;
 
