caeff11ea9e73230042ccc1f58150d14fb05b450
rapohlen <rapohlen@f2r8s11.paris.42.school>
2026-07-17 19:20:35 +0200

    ex00 done

---
+++ 05/ex00/Bureaucrat.cpp
@@ -0,0 +1,59 @@
+#include "Bureaucrat.hpp"
+#include <string>
+#include <iostream>
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade)
+	: _name(name), _grade(grade)
+{
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+}
+
+Bureaucrat::~Bureaucrat() {}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& toCopy)
+	: _name(toCopy._name), _grade(toCopy._grade)
+{}
+
+// Cannot copy name
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& op)
+{
+	if (this != &op)
+		_grade = op._grade;
+	return (*this);
+}
+
+std::ostream&	operator<<(std::ostream& out, const Bureaucrat& op)
+{
+	out << op.getName() << ", bureaucrat grade " << op.getGrade() << ".";
+	return out;
+}
+
+std::string Bureaucrat::getName() const { return _name; }
+int         Bureaucrat::getGrade() const { return _grade; }
+
+void	Bureaucrat::promote()
+{
+	if (_grade == 1)
+		throw Bureaucrat::GradeTooHighException();
+	_grade--;
+}
+
+void	Bureaucrat::demote()
+{
+	if (_grade == 150)
+		throw Bureaucrat::GradeTooLowException();
+	_grade++;
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return "Bureaucrat: grade is too high (must be 1 or higher)";
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return "Bureaucrat: grade is too low (must be 150 or lower)";
+}
---
+++ 05/ex00/Bureaucrat.hpp
@@ -0,0 +1,41 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+
+class Bureaucrat
+{
+    public:
+
+    class GradeTooHighException : public std::exception
+    {
+        public:
+        virtual const char* what() const throw();
+    };
+
+    class GradeTooLowException : public std::exception
+    {
+        public:
+        virtual const char* what() const throw();
+    };
+
+    Bureaucrat(const std::string& name = "Default", int grade = 150);
+    ~Bureaucrat();
+    Bureaucrat(const Bureaucrat& toCopy);
+    Bureaucrat& operator=(const Bureaucrat& op);
+
+    std::string getName() const;
+    int         getGrade() const;
+    void        promote();
+    void        demote();
+
+    private:
+
+    const std::string   _name;
+    int                 _grade;
+};
+
+std::ostream&	operator<<(std::ostream& out, const Bureaucrat& op);
+
+#endif /* BUREAUCRAT_HPP */
