3c8b13429229f3a590c41d6717ce0a8a941164ee
rapohlen <rapohlen@f2r7s10.paris.42.school>
2026-09-15 18:30:25 +0200

    ex01 better murloc sound, AND added test to show converted addr is usable

--- 06/ex01/main.cpp
+++ 06/ex01/main.cpp
@@ -7,13 +7,17 @@
 int	main()
 {
 	Data		*data = new Data();
+	Data		*copy;
 	uintptr_t	addr;
 
-	data->str = "Mrgrblrglrbglr";
+	data->str = "Mrglrglrglgl";
 	std::cout << "Addr of data as ptr:  " << data << "\n";
 	addr = Serialize::serialize(data);
 	std::cout << "Addr of data as uint: " << addr << "\n";
-	std::cout << "Back to ptr:          " << Serialize::deserialize(addr) << "\n";
+	copy = Serialize::deserialize(addr);
+	std::cout << "Back to ptr:          " << copy << "\n";
+	std::cout << "Data contents:        " << copy->str << "\n";
+
 	delete data;
 	return 0;
 }
