b7d7f3b160992e67d276986b8fd105ea8514b391
rapohlen <rapohlen@f2r7s11.paris.42.school>
2026-09-16 14:47:50 +0200

    CPP08 ex00 improved tests & display (&it does not work, &*it works even if it shouldn't make sense)

--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -6,12 +6,23 @@ int	main()
 {
 	std::vector<int> vec;
 
+	vec.push_back(1);
 	vec.push_back(2);
 	vec.push_back(3);
-	vec.push_back(1);
-	std::vector<int>::iterator it = easyfind(vec, 4);
-	std::cout << &it << "\n";
+	std::cout << "Searching for 1:\n";
+	std::vector<int>::iterator it = easyfind(vec, 1);
+	std::cout << "\t" << &*it << "\n";
 	if (it != vec.end())
-		std::cout << *it << "\n";
+		std::cout << "\t" << *it << "\n";
+	std::cout << "Searching for 3:\n";
+	it = easyfind(vec, 3);
+	std::cout << "\t" << &*it << "\n";
+	if (it != vec.end())
+		std::cout << "\t" << *it << "\n";
+	std::cout << "Searching for 4:\n";
+	it = easyfind(vec, 4);
+	std::cout << "\t" << &*it << "\n";
+	if (it != vec.end())
+		std::cout << "\t" << *it << "\n";
 	return 0;
 }
