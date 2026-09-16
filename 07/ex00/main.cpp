26b5997db531b1829d3a6fc2f90f89a2f6645709
rapohlen <rapohlen@f2r7s11.paris.42.school>
2026-09-16 10:53:31 +0200

    CPP07 ex00 using string to test rather than double

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -1,15 +1,16 @@
 #include "whatever.hpp"
 #include <iostream>
+#include <string>
 
 int	main()
 {
 	int	a = 2, b = 5;
-	double da = 2, db = 5;
-	std::cout << "Before, a[" << a << "], b[" << b << "], da[" << da << "], db[" << db << "]\n";
+	std::string sa = "string1", sb = "string2";
+	std::cout << "Before, a[" << a << "], b[" << b << "], sa[" << sa << "], sb[" << sb << "]\n";
 	swap(a, b);
-	swap(da, db);
-	std::cout << "After, a[" << a << "], b[" << b << "], da[" << da << "], db[" << db << "]\n";
-	std::cout << "Min a/b: " << min(a,b) << " - Min da/db: " << min(da,db) << "\n";
-	std::cout << "Max a/b: " << max(a,b) << " - Max da/db: " << max(da,db) << "\n";
+	swap(sa, sb);
+	std::cout << "After, a[" << a << "], b[" << b << "], sa[" << sa << "], sb[" << sb << "]\n";
+	std::cout << "Min a/b: " << min(a,b) << " - Min sa/sb: " << min(sa,sb) << "\n";
+	std::cout << "Max a/b: " << max(a,b) << " - Max sa/sb: " << max(sa,sb) << "\n";
 	return 0;
 }
