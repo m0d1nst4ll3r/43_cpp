f872d35530ff730b3cd252b6c5ca01de55a0dc3e
m0d1nst4ll3r <raphaelpohlen@gmail.com>
2026-09-19 10:30:42 +0200

    CPP08 ex02 init & done, quite simple, just need to research how to derive from a template class and typedef an iterator

---
+++ 08/ex02/MutantStack.hpp
@@ -0,0 +1,16 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <stack>
+
+template <typename T> class MutantStack : public std::stack<T>
+{
+	public:
+
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
+};
+
+#endif /* MUTANTSTACK_HPP */
